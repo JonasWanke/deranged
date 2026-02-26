6de3d025a1a9ad4676b3c287e17af64eec36da84
Jonas Wanke <contact@wanke.dev>
2026-02-26 21:41:47 +0100

    Upgrade to Dart 3.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   inputs = {
-    # https://github.com/NixOS/nixpkgs/pull/462771
-    nixpkgs.url = "github:nixos/nixpkgs?ref=52e1f2d2594441e36a637d3d9e79cd30426b200a";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
