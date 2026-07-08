41995a3df833ce7114eb7aa42057c573e9472980
Jonas Wanke <contact@wanke.dev>
2026-07-08 13:04:20 +0200

    Add RangeBounds factory constructors for dot-shorthands

--- lib/src/range.dart
+++ lib/src/range.dart
@@ -38,6 +38,15 @@ import 'utils.dart';
 abstract class RangeBounds<C extends Comparable<C>> {
   const RangeBounds();
 
+  factory RangeBounds.full() => const RangeFull();
+  factory RangeBounds.inclusiveOrUnbounded(C? start, C? end) =>
+      AnyRange.inclusiveOrUnbounded(start, end);
+  factory RangeBounds.exclusiveOrUnbounded(C? start, C? end) =>
+      AnyRange.exclusiveOrUnbounded(start, end);
+  factory RangeBounds.from(C start) => RangeFrom(start);
+  factory RangeBounds.to(C end) => RangeTo(end);
+  factory RangeBounds.toInclusive(C end) => RangeToInclusive(end);
+
   /// The start bound of this range.
   Bound<C> get startBound;
 
